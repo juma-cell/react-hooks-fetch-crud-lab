ab2726e5ab3f98a0a0529b4554b843b9b9b5d5b3
Juma <juma.musa@student.moringaschool.com>
2023-04-24 22:46:59 +0300

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,9 +19,19 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    //console.log(formData);
+    fetch('http://localhost:4000/questions', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json", 
+        "Accept": "application/json"},
+      body: JSON.stringify(
+          {"prompt": formData.prompt,
+          "answers": [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+          "correctIndex": formData.correctIndex})
+    
+  })
   }
-
   return (
     <section>
       <h1>New Question</h1>
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,16 +8,25 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  function handleDeleteClick(e) {
+    // Call the onDelete function and pass the question id as an argument
+    onDelete(id);
+  }
 
+  function handleChange(e) {
+    // Call the onChange function and pass the question id and selected value as arguments
+    onChange(id, e.target.value);
+  
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} id={id} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick} id={id}>Delete Question</button>
     </li>
   );
 }
